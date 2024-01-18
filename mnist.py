ec9da38953081b94f7fee2dd010df12d4c067d7d
anna <anna1024@hanmail.net>
2024-01-18 11:16:49 +0900

    0117

--- mnist.py
+++ mnist.py
@@ -1,12 +1,12 @@
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
 
 # print(train_input.shape, train_target.shape)
 # print(test_input.shape, test_target.shape)
 
-import matplotlib.pyplot as plt
 fig, axs = plt.subplots(1, 10, figsize=(10,10))
 for i in range(10):
     axs[i].imshow(train_input[i], cmap='gray_r')
