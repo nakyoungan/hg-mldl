38101a88d79a4cfba48eddca96044b38650c1033
nakyoungan <anna1024@hanmail.net>
2024-02-20 17:31:36 +0900

    240220

--- chapt3-2.py
+++ chapt3-2.py
@@ -20,61 +20,33 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
        850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 1000.0,
        1000.0])
 
-'''
-#농어데이터 산점도 찍어보기
-plt.scatter(perch_length, perch_weight)
-plt.xlabel('length')
-plt.ylabel('weight')
-plt.show()
-'''
-
-#길이가 특성이고 무게가 타깃
-#훈련세트와 테스트세트로 나누기
 train_input, test_input, train_target, test_target = train_test_split(perch_length, perch_weight, random_state=42)
 
-#사이킷런 사용을 위해 배열 2차원으로 변경
-#크기에 -1 지정하면 나머지 원소 개수로 모두 채우라는 의미 (첫번째는 원소 두번쨰는 1)
 train_input = train_input.reshape(-1,1)
 test_input = test_input.reshape(-1,1)
 
-knr = KNeighborsRegressor()
+knr = KNeighborsRegressor(n_neighbors = 3)
 lr = LinearRegression()
 
 #KNN train
 knr.fit(train_input, train_target)
 
-'''
-#R² 점수
-print("R² 는",knr.score(test_input, test_target))
+#linear regression train 
+lr.fit(train_input, train_target)
 
-#테스트세트에 대한 예측 
-test_prediction = knr.predict(test_input)
-
-#테스트세트에 대한 평균 절댓값 오차 계산  
-mae = mean_absolute_error(test_target, test_prediction)
-print("MAE는",mae)
-'''
-
-'''
-#훈련모델로 훈련 세트의 R²점수 
-print("R² 는",knr.score(train_input, train_target))
-해보니 underfitting 발생 > 해결:모델을 더 복잡하게 만들어야함
-KNN 알고리즘에서 모델을 복잡하게 만들려면? 
-> 이웃의 개수 K를 줄이기 
-> 사이킷런의 기본값인 5를 3으로 변경
-'''
-
-
-knr.n_neighbors = 3
-
-knr.fit(train_input, train_target)
-
-print(knr.predict([[100]]))
-
-distances, indexes = knr.kneighbors([[100]])
 
 plt.scatter(train_input, train_target)
-plt.scatter(train_input[indexes], train_target[indexes], marker='D')
 
-plt.scatter(100, 1033, marker='^')
+#15에서 50까지 1차 방정식
+plt.plot([15,50], [15*lr.coef_+lr.intercept_, 50*lr.coef_+lr.intercept_])
+
+#2차방정식
+train_poly = np.column_stack((train_input**2, train_input))
+test_poly = np.column_stack((test_input**2, test_input))
+lr.fit(train_poly, train_target)
+
+point = np.arange(15, 50)
+plt.plot(point, 1.01*point**2-21.6*point+116.05)
+
+plt.scatter(50, 1241.8, marker='^')
 plt.show()
