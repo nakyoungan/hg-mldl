df69620144e9991b85862fc5270f2105f0783ff2
nakyoungan <anna1024@hanmail.net>
2024-02-28 16:11:55 +0900

    240228

--- chapt5-2.py
+++ chapt5-2.py
@@ -6,6 +6,9 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
 from sklearn.model_selection import cross_validate
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 
 wine = pd.read_csv('https://bit.ly/wine-date')
 
@@ -22,5 +25,22 @@ dt.fit(sub_input, sub_target)
 print(dt.score(sub_input, sub_target))
 print(dt.score(val_input, val_target))
 
+#교차검증
 scores =  cross_validate(dt, train_input, train_target)
 print(scores)
+#검증 폴드의 점수
+print(np.mean(scores['test_score']))
+
+scores = cross_validate(dt, train_input, train_target, cv=StratifiedKFold())
+print(np.mean(scores['test_score']))
+
+params = {'min_impurity_decrease':[0.0001, 0.0002, 0.0003, 0.0004, 0.0005]}
+#그리드서치 객체 생성
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42), params, n_jobs=-1)
+#그리드서치 수행
+gs.fit(train_input, train_target)
+
+dt=gs.best_estimator_
+print(dt.score(train_input, train_target))
+#그리도 서치로 찾은 최적의 매개변수는 best_params_ 속성에 저장되어 있음
+print(gs.best_params_)
